af26dd8c9b36993a717851b59188ba03f7e65724
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-25 21:47:20 +0500

    largest number by 2 times problem

---
+++ binarysearch/largest-number-by-two-times.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+bool largest_number_by_2_times(vector<int> nums)
+{
+    int max1 = nums[0] > nums[1] ? nums[0] : nums[1];
+    int max2 = nums[1] < nums[0] ? nums[1] : nums[0];
+    for (int i = 2; i < nums.size(); i++)
+    {
+        max2 = nums[i] > max2 ? nums[i] : max2;
+        max2 = nums[i] > max1 ? max1 : max2;
+        max1 = nums[i] > max1 ? nums[i] : max1;
+    }
+
+    return max1 > max2 * 2;
+}
