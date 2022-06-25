a44a0dc6055032b3afc866bf9e7eaa04e88bd126
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-25 18:33:27 +0500

    3-6-9 problem

---
+++ binarysearch/3-6-9.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+bool containsClapNumber(int n)
+{
+    if (n % 3 == 0)
+        return true;
+    while(n)
+    {
+        int digit = n % 10;
+        if(digit == 3 || digit == 6 || digit == 9)
+            return true;
+        n /= 10;
+    }
+    return false;
+}
+
+vector<string> solve(int n)
+{
+    vector<string> numbers;
+    if (n == 0)
+        return numbers;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 || i % 6 == 0 || i % 9 == 0)
+            numbers.push_back("clap");
+        else
+        {
+            if (containsClapNumber(i) == true)
+                numbers.push_back("clap");
+            else
+                numbers.push_back(to_string(i));
+        }
+    }
+    return numbers;
+}
