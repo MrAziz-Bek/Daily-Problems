02c9fb936d7ec08455c70c80355f055c8bbf43d0
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-28 10:36:46 +0500

    square of a list

---
+++ binarysearch/square-of-a-list.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> quick_sort(vector<int> &nums)
+{
+    int n = nums.size();
+
+    int r = n - 1, l = 0, i = n - 1;
+
+    vector<int> res(n);
+
+    while (l <= r)
+    {
+        int rsq = nums[r] * nums[r];
+        int lsq = nums[l] * nums[l];
+        if (lsq > rsq)
+        {
+            res[i] = lsq;
+            l++;
+        }
+        else
+        {
+            res[i] = rsq;
+            r--;
+        }
+        i--;
+    }
+    return res;
+}
