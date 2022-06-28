00b2e51f9878d6dc58bf3af84953288958ab3883
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-28 11:07:19 +0500

    merging two sorted lists

---
+++ binarysearch/merging-two-sorted-lists.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int merging_two_sorted_lists(vector<int> &a, vector<int> &b)
+{
+    int i = 0, j = 0;
+    vector<int> res;
+    while (i < a.size() || j < b.size())
+    {
+        bool chooseA = j == b.size() || (i != a.size() && a[i] < b[j]);
+        res.push_back(chooseA ? a[i++] : b[j++]);
+    }
+    return res;
+}
