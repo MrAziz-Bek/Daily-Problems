0242f625e2acc4348400a918139f9aaa61a386fa
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-28 09:29:37 +0500

    undirectional word search

--- binarysearch/undirectional-word-search.cpp
+++ binarysearch/undirectional-word-search.cpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
-bool row_check(vector<vector<string>>& board, string word, int row, int index) {
-    for (int i = 0; i < word.length(); i++) {
+bool row_check(vector<vector<string>> &board, string word, int row, int index)
+{
+    for (int i = 0; i < word.length(); i++)
+    {
         string s;
         s.push_back(word[i]);
         if (board[row][index + i].c_str() == s)
@@ -15,8 +17,10 @@ bool row_check(vector<vector<string>>& board, string word, int row, int index) {
     }
     return true;
 }
-bool col_check(vector<vector<string>>& board, string word, int col, int index) {
-    for (int i = 0; i < word.length(); i++) {
+bool col_check(vector<vector<string>> &board, string word, int col, int index)
+{
+    for (int i = 0; i < word.length(); i++)
+    {
         string s;
         s.push_back(word[i]);
         if (board[index + i][col] == s)
@@ -26,29 +30,42 @@ bool col_check(vector<vector<string>>& board, string word, int col, int index) {
     }
     return true;
 }
-bool solve(vector<vector<string>>& board, string word) {
+bool solve(vector<vector<string>> &board, string word)
+{
     // Row
-    if (!(board[0].size() < word.length())) {
-        for (int j = 0; j < board.size(); j++) {
-            for (int i = 0; i < board[0].size(); i++) {
-                if (board[0].size() - i < word.length()) break;
+    if (!(board[0].size() < word.length()))
+    {
+        for (int j = 0; j < board.size(); j++)
+        {
+            for (int i = 0; i < board[0].size(); i++)
+            {
+                if (board[0].size() - i < word.length())
+                    break;
                 string s;
                 s.push_back(word[0]);
-                if (s == board[j][i]) {
-                    if (row_check(board, word, j, i)) return true;
+                if (s == board[j][i])
+                {
+                    if (row_check(board, word, j, i))
+                        return true;
                 }
             }
         }
     }
     // Col
-    if (!(board.size() < word.length())) {
-        for (int i = 0; i < board.size(); i += word.length()) {
-            if (board.size() - i < word.length()) break;
-            for (int j = 0; j < board[0].size(); j++) {
+    if (!(board.size() < word.length()))
+    {
+        for (int i = 0; i < board.size(); i += word.length())
+        {
+            if (board.size() - i < word.length())
+                break;
+            for (int j = 0; j < board[0].size(); j++)
+            {
                 string s;
                 s.push_back(word[0]);
-                if (s == board[i][j]) {
-                    if (col_check(board, word, j, i)) return true;
+                if (s == board[i][j])
+                {
+                    if (col_check(board, word, j, i))
+                        return true;
                 }
             }
         }
