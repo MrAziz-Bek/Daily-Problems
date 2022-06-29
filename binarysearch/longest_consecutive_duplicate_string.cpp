c2850affd2fd2976491a6258b021a9e744c42e4c
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-29 09:49:59 +0500

    longest consecutive duplicate string

--- binarysearch/longest_consecutive_duplicate_string.cpp
+++ binarysearch/longest_consecutive_duplicate_string.cpp
@@ -7,16 +7,17 @@ int longest_consecutive_duplicate_string(string s)
 {
     if (s.length() == 0)
         return 0;
-    int count = 1, prev = 0;
+    int count = 1, max = 0;
     for (int i = 0; i < s.length(); i++)
     {
         if (s[i] == s[i + 1])
             count++;
         else
         {
-            prev = count > prev ? count : prev;
+            max = count > max ? count : max;
             count = 1;
         }
     }
-    return prev;
+
+    return max;
 }
