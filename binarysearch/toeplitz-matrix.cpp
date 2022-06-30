dc568981b9465d4378a7f2eaa75dfa4d924d7c44
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-30 09:11:03 +0500

    toeplitz matrix

--- binarysearch/toeplitz-matrix.cpp
+++ binarysearch/toeplitz-matrix.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
-bool toeplitz_matrix(vector<vector<int>> matrix)
+bool toeplitz_matrix(vector<vector<int>>& matrix)
 {
     for (int i = 1; i < matrix.size(); i++)
     {
-        for (int j = 1; j < matrix[0].size(); j++)
+        for (int j = 1; j < matrix[i].size(); j++)
         {
             if (matrix[i - 1] [j - 1] < matrix[i][j])
                 return false;
         }
     }
+    cout << matrix.size();
     return true;
 }
