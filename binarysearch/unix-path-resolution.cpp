e763181cb8e569340c24b070db01175af672bd08
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-06-30 10:07:38 +0500

    unix path resolution

---
+++ binarysearch/unix-path-resolution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <stack>
+
+using namespace std;
+
+vector<string> unix_path_resolution(vector<string> &path)
+{
+    vector<string> current;
+
+    for (const auto &directory : path)
+    {
+        if (directory == "..")
+        {
+            if (!current.empty())
+                current.pop_back();
+        }
+        else if (directory != ".")
+            current.push_back(directory);
+    }
+    return current;
+}
